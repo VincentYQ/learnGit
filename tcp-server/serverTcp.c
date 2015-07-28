01dae59cb8812bfe357e8c2f282f4f1f2531e250
ytlm <1364951259@qq.com>
2015-07-28 15:09:59 +0800

    while

--- tcp-server/serverTcp.c
+++ tcp-server/serverTcp.c
@@ -26,7 +26,7 @@ int main(int argc,char *argv[])
 
 	myaddr.sin_family      = AF_INET;
 	myaddr.sin_port        = htons(myport);
-	myaddr.sin_addr.s_addr = INADDR_ANY;
+	myaddr.sin_addr.s_addr = inet_addr("0.0.0.0");
 
 	if(bind(myfd, (struct sockaddr *)&myaddr, sizeof(struct sockaddr)) == -1)
 	{
@@ -39,23 +39,27 @@ int main(int argc,char *argv[])
 		perror("listen() error!");
 		exit(1);
 	}
-	struct sockaddr_in addrClient;
-	sinsize = sizeof(struct sockaddr_in);
-	if((ClinetConn = accept(myfd,(struct sockaddr *)&addrClient, &sinsize)) == -1)
+	int count = 0;
+	while(1)
 	{
-		perror("accept() error!");
-		exit(1);
-	}
-	char sendMessage[100] = "hello word!\n";
+		struct sockaddr_in addrClient;
+		sinsize = sizeof(struct sockaddr_in);
+		if((ClinetConn = accept(myfd,(struct sockaddr *)&addrClient, &sinsize)) == -1)
+		{
+			perror("accept() error!");
+			exit(1);
+		}
+		char sendMessage[100];
+		memset(sendMessage,0,sizeof(sendMessage));
+		sprintf(sendMessage, "%d:%s",++count, "hello word");
 
-	if(send(ClinetConn, sendMessage, strlen(sendMessage),0) == -1)
-	{
-		perror("send() error!");
-		exit(1);
+		if(send(ClinetConn, sendMessage, strlen(sendMessage),0) == -1)
+		{
+			perror("send() error!");
+			exit(1);
+		}
 	}
 
 	close(myfd);
 	return 0;
 }
-
-
